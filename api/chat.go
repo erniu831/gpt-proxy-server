c6436e32ab80213176ecab34face2db5b2e759b3
erniu831 <13842642650@163.com>
2023-04-01 19:11:07 +0800

    .

--- api/chat.go
+++ api/chat.go
@@ -6,8 +6,6 @@ import (
 	chatService "quick-talk/service/chat"
 
 	"github.com/gin-gonic/gin"
-
-	gogpt "github.com/sashabaranov/go-openai"
 )
 
 // UserRegister 用户注册接口
@@ -16,26 +14,6 @@ func ChatCompletion(c *gin.Context) {
 	if err := c.ShouldBind(&service); err == nil {
 		fmt.Println(fmt.Sprintf("%+v", service))
 		res, err := gpt.Completion(c, service)
-		if err != nil {
-			c.JSON(200, res)
-		}
-	} else {
-		c.JSON(200, ErrorResponse(err))
-	}
-}
-
-func ChatProcess(c *gin.Context) {
-	var service chatService.ChatProcessService
-	if err := c.ShouldBind(&service); err == nil {
-		fmt.Println(fmt.Sprintf("%+v", service))
-		res, err := gpt.Completion(c, chatService.CompletionService{
-			Messages: []gogpt.ChatCompletionMessage{
-				{
-					Role:    "user",
-					Content: service.Prompt,
-				},
-			},
-		})
 		if err != nil {
 			c.JSON(200, res)
 		}
